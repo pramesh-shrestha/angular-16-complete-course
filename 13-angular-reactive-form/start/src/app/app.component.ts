ec3be87d5977d4f8d1e9712bc351c912e4bfaaab
Pramesh Shrestha <prmsstha22@gmail.com>
2024-06-09 19:52:40 +0200

    initial commit

--- 13-angular-reactive-form/start/src/app/app.component.ts
+++ 13-angular-reactive-form/start/src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
   title = 'template-driven-form';
-} 
+  registrationForm: FormGroup;
+  ngOnInit(): void {
+    this.registrationForm = new FormGroup({
+      fullName: new FormGroup({
+        firstName: new FormControl(null, Validators.required),
+        lastName: new FormControl(null, Validators.required),
+      }),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+
+      usernameAndDate: new FormGroup({
+        username: new FormControl(null, Validators.required),
+        date: new FormControl(null, Validators.required),
+      }),
+      gender: new FormControl('male'),
+
+      address: new FormGroup({
+        street: new FormControl(null, Validators.required),
+        country: new FormControl('Nepal'),
+        city: new FormControl(null, Validators.required),
+        region: new FormControl(null),
+        postal: new FormControl(null)
+      }),
+      skills: new FormArray([
+        new FormControl(null)
+      ]),
+
+      experiences: new FormArray([])
+    })
+  }
+
+  protected readonly onsubmit = onsubmit;
+
+  onSubmit() {
+    console.log(this.registrationForm);
+  }
+
+  get skills(): FormArray{
+    return this.registrationForm.get('skills') as FormArray;
+  }
+
+  onAddSkills() {
+    const control =  new FormControl(null);
+    (this.registrationForm.get('skills') as FormArray).push(control);
+  }
+
+  onDeleteSkill(index: number) {
+    (this.registrationForm.get('skills') as FormArray).removeAt(index);
+  }
+}
--- 13-angular-reactive-form/start/src/app/app.module.ts
+++ 13-angular-reactive-form/start/src/app/app.module.ts
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
